59b02585ee24efc09dd42fa3eedc4dd90a9a12d5
Lucas Gabriel <lgbrito@protonmail.com>
2024-11-06 20:10:21 -0300

    Changing stats to show data like a csv

--- utils/stats.go
+++ utils/stats.go
@@ -7,16 +7,9 @@ import (
 
 func PrintGCStats(memStats runtime.MemStats) {
 	runtime.ReadMemStats(&memStats)
-	fmt.Println(" ")
-	fmt.Println("MALLOC:", memStats.Mallocs)
-	fmt.Println("FREES:", memStats.Frees)
-	fmt.Println("HEAP ALLOC:", memStats.HeapAlloc)
-	fmt.Println("HEAP SYS:", memStats.HeapSys)
-	fmt.Println("HEAP RELEASED:", memStats.HeapReleased)
-	fmt.Println("NEXT GC:", memStats.NextGC)
-	fmt.Println("LAST GC:", memStats.LastGC)
-	fmt.Println("PAUSE TOTAL NS:", memStats.PauseTotalNs)
-	fmt.Println("NUM GC:", memStats.NumGC)
-	fmt.Println("NUM FORCED GC:", memStats.NumForcedGC)
-	fmt.Println("-------------------------------------")
+	fmt.Println("")
+	fmt.Print(memStats.Mallocs, ",", memStats.Frees, ",",
+		memStats.HeapAlloc, ",", memStats.HeapSys, ",", memStats.HeapReleased, ",", memStats.NextGC, ",",
+		memStats.LastGC, ",", memStats.PauseTotalNs, ",", memStats.NumGC, ",", memStats.NumForcedGC, '\n')
+
 }
