2b086eea573de82f138e4889ceec841fcaa345f8
LucasG42 <lucasilveira865@gmail.com>
2025-01-23 19:55:10 -0300

    Updating run.sh to run gogc until value 1000

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
-parmnemonics=true
+parmnemonics=false
 xalan=false
-fjkmeans=false
+fjkmeans=true
 
-header="GOGC,Mallocs,Frees,HeapAlloc,HeapSys,HeapReleased,NextGC,LastGC,PauseTotalNs,NumGC,NumForcedGC,GCCPUFraction,TotalTimeExecutionInMilliseconds"
+header="GOGC,Mallocs,Frees,HeapAlloc,HeapSys,HeapReleased,NextGC,LastGC,PauseTotalNs,NumGC,NumForcedGC,GCCPUFraction,TotalTimeExecutionInNanoseconds"
 
 if [ $parmnemonics = true ]; then
 
@@ -13,11 +13,27 @@ echo "Running par-mnemonics"
 echo $header > ./output/parmnemonics_results.csv  
 cd benchmarks/
 
-export GOGC=25  && go test -bench=. -benchtime=10x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
-export GOGC=50  && go test -bench=. -benchtime=10x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
-export GOGC=100 && go test -bench=. -benchtime=10x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
-export GOGC=200 && go test -bench=. -benchtime=10x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
-export GOGC=400 && go test -bench=. -benchtime=10x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=50   && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=100  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=150  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=200  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=250  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=300  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=350  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=400  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=450  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=500  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=550  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=600  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=650  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=700  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=750  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=800  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=850  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=900  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=950  && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+export GOGC=1000 && go test -bench=. -benchtime=100x $PWD/par-mnemonics/  && cat $PWD/par-mnemonics/results.csv >> ../output/parmnemonics_results.csv 
+
 
 rm $PWD/par-mnemonics/results.csv
 cd ../
@@ -32,11 +48,27 @@ echo $header > ./output/xalan_results.csv
 
 cd benchmarks/
 
-export GOGC=25  && go test -bench=. -benchtime=10x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
-export GOGC=50  && go test -bench=. -benchtime=10x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
-export GOGC=100 && go test -bench=. -benchtime=10x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
-export GOGC=200 && go test -bench=. -benchtime=10x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
-export GOGC=400 && go test -bench=. -benchtime=10x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=50   && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=100  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=150  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=200  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=250  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=300  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=350  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=400  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=450  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=500  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=550  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=600  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=650  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=700  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=750  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=800  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=850  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=900  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=950  && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+export GOGC=1000 && go test -bench=. -benchtime=100x $PWD/xalan/  && cat $PWD/xalan/results.csv >> ../output/xalan_results.csv 
+
 
 rm $PWD/xalan/results.csv
 
@@ -51,11 +83,27 @@ echo $header > ./output/fjkmeans_results.csv
 
 cd benchmarks/
 
-export GOGC=25  && go test -bench=. -benchtime=10x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
-export GOGC=50  && go test -bench=. -benchtime=10x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
-export GOGC=100 && go test -bench=. -benchtime=10x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
-export GOGC=200 && go test -bench=. -benchtime=10x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
-export GOGC=400 && go test -bench=. -benchtime=10x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=50   && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=100  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=150  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=200  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=250  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=300  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=350  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=400  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=450  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=500  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=550  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=600  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=650  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=700  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=750  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=800  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=850  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=900  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=950  && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+export GOGC=1000 && go test -bench=. -benchtime=100x $PWD/fj-kmeans/  && cat $PWD/fj-kmeans/results.csv >> ../output/fjkmeans_results.csv 
+
 
 rm $PWD/fj-kmeans/results.csv
 
