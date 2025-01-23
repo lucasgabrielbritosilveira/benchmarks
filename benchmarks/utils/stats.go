730d5366da2811c620ced10812d69b7b7c1cca14
LucasG42 <lucasilveira865@gmail.com>
2025-01-23 19:58:48 -0300

    Updating stats to use ExecutionTimeInNanoseconds

--- benchmarks/utils/stats.go
+++ benchmarks/utils/stats.go
@@ -22,6 +22,6 @@ func GenerateExecutionData(memStats *runtime.MemStats, totalTime time.Duration) 
 		strconv.FormatUint(memStats.Mallocs, 10), strconv.FormatUint(memStats.Frees, 10),
 		strconv.FormatUint(memStats.HeapAlloc, 10), strconv.FormatUint(memStats.HeapSys, 10), strconv.FormatUint(memStats.HeapReleased, 10), strconv.FormatUint(memStats.NextGC, 10),
 		strconv.FormatUint(memStats.LastGC, 10), strconv.FormatUint(memStats.PauseTotalNs, 10), strconv.FormatUint(uint64(memStats.NumGC), 10), strconv.FormatUint(uint64(memStats.NumForcedGC), 10),
-		strconv.FormatFloat(memStats.GCCPUFraction, 'f', 2, 64), strconv.FormatInt(totalTime.Milliseconds(), 10),
+		strconv.FormatFloat(memStats.GCCPUFraction, 'f', 2, 64), strconv.FormatInt(totalTime.Nanoseconds(), 10),
 	}
 }
